78e6d38520b3460bfc251acae8be599dc7041a09
Çağdaş Aydemir <ac.cagdasaydemir@gmail.com>
2022-08-09 18:56:30 +0300

    fix centering

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
- <CalculatorComp></CalculatorComp>
- 
+  <CalculatorComp></CalculatorComp>
+
 </template>
 
 <script>
@@ -11,17 +11,18 @@ import CalculatorComp from './components/CalculatorComp.vue'
 export default {
   name: 'App',
   components: {
-  
+
     CalculatorComp
-}
+  }
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@200&display=swap');
+
 #app {
-font-family: 'Work Sans', sans-serif;
-text-rendering: optimizeLegibility;
+  font-family: 'Work Sans', sans-serif;
+  text-rendering: optimizeLegibility;
   font-weight: 200;
   font-size: 30px;
   -webkit-font-smoothing: antialiased;
@@ -29,21 +30,23 @@ text-rendering: optimizeLegibility;
   text-align: center;
   margin-top: 60px;
   box-sizing: border-box;
-  
+
 }
-body{
-   background: linear-gradient(
-    90deg,
-    #3b3c5c,
-    #5d486e,
-    #82537a,
-    #a65e7f,
-    #c86d7e,
-    #e38078,
-    #f69870,
-    #ffb56b
-  );
+
+body {
+  background: linear-gradient(90deg,
+      #3b3c5c,
+      #5d486e,
+      #82537a,
+      #a65e7f,
+      #c86d7e,
+      #e38078,
+      #f69870,
+      #ffb56b);
   background-size: cover;
- 
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
